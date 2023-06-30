bd824a39b69f8182ad064b48795cb5114a5aa86b
Harsh Deepanshu <harshdeepanshustrix@gmail.com>
2023-06-30 21:15:41 +0530

    add some assignments!

--- LeetCode Cpp/valid_para.cpp
+++ LeetCode Cpp/valid_para.cpp
@@ -4,27 +4,45 @@ using namespace std;
 // we are compairing ascii as if x has this ascii value and x+1 has this ascii value then only it will return so basically opening(x) and closing(x+1) bracket ascii value
 // Better way use map(STL)
 
-string isValid(string s)
+bool isValid(string s)
 {
-    string status = "";
-        for (int i = 0; i < 1; i++ )
-        {
-            cout << s[s.size()-2] << " - " << s[s.size()-1] <<endl;
-            if(s[s.size()-2]  == s[s.size()-1] - 1)
+    unordered_map<char, char> u_brackets;
+    u_brackets.insert({'(', ')'});
+    u_brackets.insert({'{', '}'});
+    u_brackets.insert({'[', ']'});
+    bool status = false;
+    for (char key : s)
+    {
+            if( s.size() % 2 == 0){
+                if (u_brackets.find(s[s.size()-2])->second == s[s.size() - 1])
             {
-                status = "true";
-                
-            }else
-            {
-                status = "false";
+                status = true;
             }
-        }
-        return status;
+            else
+            {
+                status = false;
+                break;
+            }
+            }else{
+                status = false;
+                break;
+            }
+            
+    }
+    return status;
 }
 
-int main()
-{
-    string x = "(){}[]";
-    cout << isValid(x) << endl;
-    return 0;
-}
+    int main()
+    {
+        string x = "(){}[]";
+
+        if (isValid(x))
+        {
+            cout << "True" << endl;
+        }
+        else
+        {
+            cout << "False" << endl;
+        }
+        return 0;
+    }
